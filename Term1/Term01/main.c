4dc0c36b32092ca1279aae347f681f40c18571ae
supernova09 <heeseong98@naver.com>
2018-06-02 05:14:29 +0900

    apply gitignore

---
+++ Term1/Term01/main.c
@@ -0,0 +1,28 @@
+#pragma once
+#include <stdio.h>
+#include "csvParser.h"
+#include "GUI.h"
+
+#define INF 10000
+
+Station stations[STATION_NUM];
+double weight[STATION_NUM][STATION_NUM];
+
+void InitGraph();
+
+int main(void) {
+	Parse(stations);
+	InitGraph();
+	InitGUI();
+	//Title();
+}
+
+void InitGraph() {
+	for (int i = 0; i < STATION_NUM; i++) {
+		for (int j = 0; j < STATION_NUM; j++)
+			weight[i][j] = i == j ? 0 : INF;
+	}
+	TransferParse(weight);
+
+}
+
